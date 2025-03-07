17d09a7d3aab85115e8318857f9ed518df4d2730
Ryan <ryanxdlee@gmail.com>
2025-03-07 10:23:24 +0800

    implement auth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards, Get, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { Public } from './auth.constants';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() request: any) {
+    return request.user;
+  }
+}
--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,46 +1,51 @@
-import { Controller, Get, Post, Body, Param, Req, UseGuards, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Req, UseGuards, Put, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
+import { Public } from 'src/auth/auth.constants';
 // import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  
-  @Post()
-    async createUser(@Body() createUserDto: CreateUserDTO) {
-      await this.usersService.createUser(createUserDto);
-      return {
-        success: true,
-        message: 'user created successfully',
-      };
-    }
 
-  @Get(':id')
-  async getUserById(@Param('id') id: number) {
-    await this.usersService.getUserById(id);
+  @Public()
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() createUserDto: CreateUserDTO) {
     return {
       success: true,
-      message: 'user retrived',
-    }
+      data: await this.usersService.createUser(createUserDto),
+    };
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async getUserById(@Param('id') id: number) {
+    return {
+      success: true,
+      data: await this.usersService.getUserById(id),
+    };
   }
 
   @Put(':id')
-  async updateUser(@Param('id') id: number, @Body() updateUserDto: UpdateUserDTO) {
-    await this.usersService.updateUser(id, updateUserDto);
+  @HttpCode(HttpStatus.OK)
+  async updateUser(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDTO,
+  ) {
     return {
       success: true,
-      message: 'user updated',
+      data: await this.usersService.updateUser(id, updateUserDto),
     };
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async deleteUser(@Param('id') id: number) {
-    await this.usersService.deleteUser(id);
     return {
       success: true,
-      message: 'user deleted',
+      data: await this.usersService.deleteUser(id),
     };
   }
 }
